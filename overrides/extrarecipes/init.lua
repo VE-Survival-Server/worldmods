dd74f3b0dc058bf92f7ee1cc5170951ebc7935da
2424barrier <2424barrier.minetest@gmail.com>
2025-07-19 22:22:07 +0100

    added old separating recipe for bees-wax

--- overrides/extrarecipes/init.lua
+++ overrides/extrarecipes/init.lua
@@ -1,1 +1,5 @@
 technic.register_grinder_recipe({input={"technic:stone_dust"},output="default:silver_sand"})
+
+if (core.get_modpath("dye") and core.get_modpath("bees")) then
+	technic.register_separating_recipe({ input = {"bees:wax 1"}, output = {"basic_materials:oil_extract 2","dye:yellow 1"}})
+end
