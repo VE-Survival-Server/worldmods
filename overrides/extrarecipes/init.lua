b53fc031d7c310110d90e1ca5746af11c6e098c8
2424barrier <2424barrier.minetest@gmail.com>
2025-07-26 17:38:17 +0100

    added dirt_with_grass recipe, updated mobs_redo

--- overrides/extrarecipes/init.lua
+++ overrides/extrarecipes/init.lua
@@ -1,5 +1,13 @@
 technic.register_grinder_recipe({input={"technic:stone_dust"},output="default:silver_sand"})
 
+core.register_craft({
+	type = "shapeless",
+	output = "default:dirt_with_grass 1",
+	recipe = {
+		"default:grass_1",
+		"default:dirt",
+	},
+})
 if (core.get_modpath("dye") and core.get_modpath("bees")) then
 	technic.register_separating_recipe({ input = {"bees:wax 1"}, output = {"basic_materials:oil_extract 2","dye:yellow 1"}})
 end
